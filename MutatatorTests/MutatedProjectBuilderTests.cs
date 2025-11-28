2834de88cee1cd52302c046ae14ee533c3364199
James Lamont <jamesnplamont@gmail.com>
2025-11-28 23:12:27 +0000

    does this constitute testing???

---
+++ MutatatorTests/MutatedProjectBuilderTests.cs
@@ -0,0 +1,10 @@
+﻿namespace MutatorTests;
+
+/// <summary>
+/// Now, this class is entirly testable, but the thing is, its 23:09, and i cba. which means it probably wont ever be tested,
+/// because what are the chances i come back to this to do it later? Id wager fairly low. maybe if i put a TODO here, itll up the chances.
+/// </summary>
+internal class MutatedProjectBuilderTests
+{
+    
+}
