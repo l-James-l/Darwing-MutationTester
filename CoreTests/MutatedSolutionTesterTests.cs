346a3d08e51d46b9b6b622a22f7f8e370f632317
James Lamont <jamesnplamont@gmail.com>
2026-02-20 12:03:34 +0000

    mutation tester and profile dersializer

---
+++ CoreTests/MutatedSolutionTesterTests.cs
@@ -0,0 +1,120 @@
+﻿using Core;
+using Core.IndustrialEstate;
+using Core.Interfaces;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Models;
+using Models.Enums;
+using Models.Events;
+using Models.SharedInterfaces;
+using Mutator;
+using Mutator.MutationImplementations;
+using NSubstitute;
+using System.Diagnostics;
+
+namespace CoreTests;
+
+public class MutatedSolutionTesterTests
+{
+    private IEventAggregator _eventAggregator;
+    private IMutationDiscoveryManager _discoveryManager;
+    private IProcessWrapperFactory _processFactory;
+    private IMutationSettings _settings;
+    private IStatusTracker _statusTracker;
+    private IProcessWrapper _mockProcess;
+    private MutatedSolutionTester _mutatedSolutionTester;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _eventAggregator = Substitute.For<IEventAggregator>();
+        _discoveryManager = Substitute.For<IMutationDiscoveryManager>();
+        _processFactory = Substitute.For<IProcessWrapperFactory>();
+        _settings = Substitute.For<IMutationSettings>();
+        _statusTracker = Substitute.For<IStatusTracker>();
+        _mockProcess = Substitute.For<IProcessWrapper>();
+
+        // Default setup: Status tracker allows operation
+        _statusTracker.TryStartOperation(DarwingOperation.TestMutants).Returns(true);
+        _settings.SolutionPath.Returns(@"C:\Solution\App.sln");
+        _settings.TestRunTimeout.Returns(1000);
+        _settings.MutationTestTimeoutScaler.Returns(1.5);
+        _processFactory.Create(Arg.Any<ProcessStartInfo>()).Returns(_mockProcess);
+
+        _mutatedSolutionTester = new MutatedSolutionTester(_eventAggregator, _discoveryManager, _processFactory, _settings, _statusTracker);
+    }
+
+    [Test]
+    public void RunTests_WhenProcessTimesOut_MutantIsKilled()
+    {
+        // Arrange
+        var mutant = CreateAvailableMutant("123");
+        _discoveryManager.DiscoveredMutations.Returns(new List<DiscoveredMutation> { mutant });
+        _settings.SkipTestingNoActiveMutants.Returns(true); // Skip preliminary run for simplicity
+
+        // StartAndAwait returning false simulates a timeout
+        _mockProcess.StartAndAwait(Arg.Any<double>()).Returns(false);
+        _mockProcess.Output.Returns([]);
+
+        // Act
+        _mutatedSolutionTester.RunTestsOnMutatedSolution();
+
+        // Assert
+        Assert.That(mutant.Status, Is.EqualTo(MutantStatus.Killed));
+        _statusTracker.Received().FinishOperation(DarwingOperation.TestMutants, true);
+    }
+
+    [Test]
+    public void RunTests_PassesCorrectMutantIdInEnvironment()
+    {
+        // Arrange
+        var mutant = CreateAvailableMutant("ID_999");
+        _discoveryManager.DiscoveredMutations.Returns(new List<DiscoveredMutation> { mutant });
+        _settings.SkipTestingNoActiveMutants.Returns(true);
+        _mockProcess.StartAndAwait(Arg.Any<double>()).Returns(true);
+        _mockProcess.Success.Returns(true); // Process ran but mutant survived
+
+        // Act
+        _mutatedSolutionTester.RunTestsOnMutatedSolution();
+
+        // Assert
+        _processFactory.Received().Create(Arg.Is<ProcessStartInfo>(info =>
+            info.EnvironmentVariables[Annotations.ActiveMutationIndex] == "ID_999"
+        ));
+    }
+
+    [Test]
+    public void RunTests_ScalesTimeoutBasedOnInitialRun()
+    {
+        // Arrange
+        // Simulate receipt of InitialTestRunCompleteEvent
+        var initialInfo = new InitialTestRunInfo { InitialRunDuration = TimeSpan.FromSeconds(100) };
+        var testInfoEvent = Substitute.For<InitialTestRunCompleteEvent>();
+        _eventAggregator.GetEvent<InitialTestRunCompleteEvent>().Returns(testInfoEvent);
+
+        // Capture the subscription and invoke it
+        testInfoEvent.When(x => x.Subscribe(Arg.Any<Action<InitialTestRunInfo>>(),
+            Arg.Any<ThreadOption>())).Do(x => x.Arg<Action<InitialTestRunInfo>>().Invoke(initialInfo));
+
+        _mutatedSolutionTester.StartUp();
+        var mutant = CreateAvailableMutant("1");
+        _discoveryManager.DiscoveredMutations.Returns(new List<DiscoveredMutation> { mutant });
+        _settings.SkipTestingNoActiveMutants.Returns(true);
+        _settings.MutationTestTimeoutScaler.Returns(2.0);
+
+        // Act
+        _mutatedSolutionTester.RunTestsOnMutatedSolution();
+
+        // Assert: 100ms * 2.0 scaler = 200ms
+        _mockProcess.Received().StartAndAwait(Arg.Is<TimeSpan>(x => x.TotalSeconds == 200));
+    }
+
+    private DiscoveredMutation CreateAvailableMutant(string id)
+    {
+        // Mocking the SyntaxNode parts might be needed depending on your DiscoveredMutation constructor
+        return new DiscoveredMutation(new SyntaxAnnotation("key", id), SyntaxFactory.EmptyStatement(), SyntaxFactory.EmptyStatement(), SyntaxFactory.EmptyStatement(), _eventAggregator, 0, 0)
+        {
+            Status = MutantStatus.Available,
+        };
+    }
+}
---
+++ CoreTests/Startup/SolutionProfileDeserializerTests.cs
@@ -0,0 +1,56 @@
+﻿using Core;
+using Models;
+using NSubstitute;
+using System.Reflection;
+
+namespace CoreTests.Startup;
+
+[TestFixture]
+public class SolutionProfileDeserializerTests
+{
+    private IMutationSettings _mutationSettings;
+    private SolutionProfileDeserializer _sut;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mutationSettings = Substitute.For<IMutationSettings>();
+        _sut = new SolutionProfileDeserializer(_mutationSettings);
+    }
+
+    [Test]
+    public void AssignSettingsFromProfile_MapsAllPropertiesToSettings()
+    {
+        // 1. Arrange: Create a profile with dummy data for every property
+        var profile = new SolutionProfileData
+        {
+            TestProjects = new List<string> { "TestA" },
+            IgnoreProjects = new List<string> { "IgnoreA" },
+            SingleMutantPerLine = true,
+            BuildTimeout = 999,
+            MutationTestTimeoutScaler = 2.5,
+            DefaultGitComparisonBranch = "develop"
+        };
+
+        // 2. Act: Invoke the private method
+        MethodInfo? method = typeof(SolutionProfileDeserializer).GetMethod("AssignSettingsFromProfile",
+            BindingFlags.NonPublic | BindingFlags.Instance);
+
+        method?.Invoke(_sut, [profile]);
+
+        // 3. Assert: Use Reflection to verify the Setter was called on the mock for every property
+        PropertyInfo[] profileProperties = typeof(SolutionProfileData).GetProperties();
+
+        foreach (PropertyInfo prop in profileProperties)
+        {
+            object? expectedValue = prop.GetValue(profile);
+
+            // This dynamically checks that the property on the mock was set to the expectedValue
+            // We use Received() to verify the setter was actually called
+            _mutationSettings.Received().GetType().GetProperty(prop.Name)?.SetValue(_mutationSettings, expectedValue);
+
+            // Note: If property names differ between Profile and Settings, 
+            // you'll need a mapping dictionary or custom logic here.
+        }
+    }
+}
